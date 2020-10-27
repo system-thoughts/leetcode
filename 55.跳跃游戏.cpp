592d2d292aa1e8c2cfd6f8c4b4d531b6139e3814
system-thoughts <lvying.system.thoughts@gmail.com>
2020-10-28 00:49:48 +0800

    version2: 注释见文件
    Signed-off-by: system-thoughts <lvying.system.thoughts@gmail.com>

--- 55.跳跃游戏.cpp
+++ 55.跳跃游戏.cpp
@@ -2,28 +2,25 @@
  * @lc app=leetcode.cn id=55 lang=cpp
  *
  * [55] 跳跃游戏
+ * 1. 如果作为起跳点的格子跳跃距离是3，随后的3个格子都是可以作为起跳点往后跳
+ * 2. 可以对每个能作为起跳点的格子都尝试跳跃，更新能够跳跃到的最大距离
+ * 3. 如果能够跳跃的最大距离能够达到数组终点，则成功；若最大距离都到不了下次跳跃的位置，则失败。
  */
 
 // @lc code=start
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        // 起点即终点，不用跳
-        if (nums.size() == 1)
-            return true;
-        return jump(nums, 0, nums[0]);
-    }
-    bool jump(vector<int>& nums, int pos, int len) {
-        // 如果当前位置，能够跳的最大距离能够到达数组末尾，则肯定能够跳到数组末尾
-        if (pos + len >= nums.size() - 1) {
-            return true;
-        }
-        for (int i = len; i > 0; i--) {
-            // 尝试使用各种跳跃长度进行跳跃，当然跳跃长度不能为0（原地不动）
-            if (jump(nums, pos + i, nums[pos + i]))
+        int maxDistance = 0;
+        for (int pos = 0; pos < nums.size(); pos++) {
+            // 最大距离达到不了下次跳跃的位置
+            if (maxDistance < pos)
+                return false;
+            maxDistance = max(maxDistance, pos + nums[pos]);
+            if (maxDistance >= nums.size() - 1)
                 return true;
         }
-        return false;
+        return true;
     }
 };
 // @lc code=end
