998dd3b5c8ee6a42b9405a4d911153a9bc09342d
lvying <lvying.system.thoughts@gmail.com>
2020-10-28 22:58:53 +0800

    NO.23 use priority_queue
    Signed-off-by: lvying <lvying.system.thoughts@gmail.com>

--- 23.合并k个升序链表.cpp
+++ 23.合并k个升序链表.cpp
@@ -15,35 +15,33 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+class compare {
+public:
+    bool operator() (const ListNode& a, const ListNode& b) const {return a.val > b.val;}
+};
+
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         ListNode* res = new ListNode();
-        ListNode* tail = res;
-        for (auto list : lists) {
-            mergeTwoLists(res->next, list, tail);
-        }
-        return res->next;       
-    }
-    void mergeTwoLists(ListNode* listA, ListNode* listB, ListNode *tail) {
-        while (listA && listB)
+        ListNode *tail = res;
+        priority_queue<ListNode, vector<ListNode>, compare> pq;
+        for (auto list : lists)
         {
-            if (listA->val < listB->val) {
-                tail->next = new ListNode(listA->val);
-                listA = listA->next;
-            } else {
-                tail->next = new ListNode(listB->val);
-                listB = listB->next;
+            while (list)
+            {
+                pq.push(*list);
+                list = list->next;
             }
-            tail = tail->next;
         }
-        ListNode *remain = listA ? listA : listB;
-        while (remain)
+        while (!pq.empty())
         {
-            tail->next = new ListNode(remain->val);
-            remain = remain->next;
+            tail->next = new ListNode(pq.top().val);
+            pq.pop();
             tail = tail->next;
         }
+        return res->next;
     }
 };
 // @lc code=end
