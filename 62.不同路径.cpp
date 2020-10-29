2c008635f41759652b85873e2cb937fbd42614a1
system-thoughts <lvying.system.thoughts@gmail.com>
2020-10-29 12:43:49 +0800

    NO.62 same DP function, 正向推导，最后得到最终结果，避免递归调用的栈深度
    Signed-off-by: system-thoughts <lvying.system.thoughts@gmail.com>

--- 62.不同路径.cpp
+++ 62.不同路径.cpp
@@ -2,17 +2,33 @@
  * @lc app=leetcode.cn id=62 lang=cpp
  *
  * [62] 不同路径
- * 超时是应该的，想想m和n较大时，这个栈得有多深
+ * 不使用递归，根据动态方程正向推导得到最终结果
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int uniquePaths(int m, int n) {
-        if (m == 1 || n == 1)
-            return 1;
-        return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+    int uniquePaths(int m, int n)
+    {
+        int i, j;
+        vector<vector<int>> path(m, vector<int>(n, 0));
+        for (i = 0; i < n; i++)
+        {
+            path[0][i] = 1;
+        }
+        for (i = 1; i < m; i++)
+        {
+            path[i][0] = 1;
+        }
+        for (i = 1; i < m; i++)
+        {
+            for (j = 1; j < n; j++)
+            {
+                path[i][j] = path[i - 1][j] + path[i][j - 1];
+            }
+        }
+        return path[m - 1][n - 1];
     }
 };
 // @lc code=end
-
