82f9f7d034ca52189c2507efa01a29321b08be3e
system-thoughts <lvying.system.thoughts@gmail.com>
2020-10-31 12:41:00 +0800

    NO.215 use cpp sort function
    Signed-off-by: system-thoughts <lvying.system.thoughts@gmail.com>

---
+++ 215.数组中的第k个最大元素.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=215 lang=cpp
+ *
+ * [215] 数组中的第K个最大元素
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() - k];
+    }
+};
+// @lc code=end
+
