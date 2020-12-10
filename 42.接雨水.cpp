70c06aae98d85733c3a9c2896d99d98ef7490dfd
system-thoughts <lvying.system.thoughts@gmail.com>
2020-12-10 20:54:06 +0800

    NO.42 重写代码和注释，使得逻辑更加清楚

--- 42.接雨水.cpp
+++ 42.接雨水.cpp
@@ -2,8 +2,14 @@
  * @lc app=leetcode.cn id=42 lang=cpp
  *
  * [42] 接雨水
- * 使用栈保存墙的高度，如果当前墙的高度小于栈顶墙的高度，则将当前墙的高度入栈，因为这样后续可以有雨水存放的空间
- * 当前的墙高度若大于栈顶墙的高度，则将栈顶的墙出栈，计算积水容量
+ * 使用单调递减栈保存墙的高度
+ * 1. 只有比栈顶更高的墙要入栈的时候，雨水才能被接住
+ *  1.1 此时栈顶元素出栈，该元素的高度记为base，新的栈顶元素便成为接水坑位的左侧
+ *  1.2 坑位的长度 = l - r - 1
+ *  1.3 坑位的高度 = min(height[l], height[r]) - base
+ *  1.4 即便是高度相同的墙，应该直接入栈，因为他们都能够一起承接雨水
+ *  1.5 出栈直到当前栈顶元素比入栈元素要大，保持了单调性
+ * 2. 入栈该元素
  */
 
 // @lc code=start
@@ -12,27 +18,19 @@ public:
     int trap(vector<int>& height) {
         // 左边界
         stack<int> bound;
-        int h;
         int capacity = 0;
         for (int i = 0; i < height.size(); i++) {
-            while (!bound.empty())
-            {
-                h = height[bound.top()];
-                if (height[i] > h) {
-                    bound.pop();
-                    if (bound.empty()) {
-                        break;
-                    } else {
-                        capacity += (min(height[i], height[bound.top()]) - h) * (i - bound.top() - 1);
-                    }
-                } else {
-                    bound.push(i);
+            while (!bound.empty() && height[i] > height[bound.top()]) {
+                int base = height[bound.top()];
+                bound.pop();
+                if (bound.empty())
                     break;
-                }
+                int left = bound.top();
+                int right = i;
+                int h = min(height[left], height[right]) - base;
+                capacity += (right - left - 1) * h;
             }
-            if (bound.empty()) {
-                bound.push(i);
-            }
+            bound.push(i);
         }
         return capacity;
     }
